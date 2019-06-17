720067eb524a652e444027ef35ba8859941b59aa
Sean Belverstone <seanbelverstone@gmail.com>
2019-06-16 19:31:48 -0500

    Forgot to mention in the last commit that I added styling to the random number so it stands out. Created a setTotalScore function and called that within the button click event. Also included the totalScore id in the styling of random number so it matches.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 /* Psuedocoding for game
 When start button is pressed, generate random number (between 19 and 120) into the random number div.
+Also set wins, losses and total score to 0. DONE
 Assign an individual random number (between 1 and 12) to each crystal and hide them.
 Show number when crystal is clicked and add it to player's total score.
 When total score is equal to the random number, update wins by 1 and generate another number.
@@ -9,7 +10,7 @@ var maximum = 120;
 var minimum = 19;
 var randomNumber = (Math.floor(Math.random() * (maximum - minimum + 1) ) + minimum);
 console.log(randomNumber);
-var totalScore;
+var totalScore = 0;
 var wins = 0;
 var losses = 0;
 
@@ -25,9 +26,14 @@ function setRandomNumber() {
     $("#randomNumber").append(randomNumber);
 }
 
+function setTotalScore() {
+    $("#totalScore").append(totalScore);
+}
+
 $(".btn").one("click", function startGame() {
     setWins();
     setLosses();
     setRandomNumber();
+    setTotalScore();
 });
 
